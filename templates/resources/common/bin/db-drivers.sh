e6790d62c10c65549f1dd6005345b6947af8111d
Oleksandr Korniienko <okorniienko@xebialabs.com>
2019-10-10 16:16:50 +0200

    DEPL-14939 Task Queue Implementation - Add detection of mq driver (RabbitMQ/ActiveMQ) depending on driver name.

--- templates/resources/common/bin/db-drivers.sh
+++ templates/resources/common/bin/db-drivers.sh
@@ -13,3 +13,10 @@ curl http://repo1.maven.org/maven2/mysql/mysql-connector-java/${MYSQL_VERSION}/m
 curl http://repo1.maven.org/maven2/com/h2database/h2/${H2_VERSION}/h2-${H2_VERSION}.jar -o ${APP_ROOT}/db-libs/h2-${H2_VERSION}.jar -f
 curl https://jdbc.postgresql.org/download/postgresql-${POSTGRESQL_VERSION}.jar -o ${APP_ROOT}/db-libs/postgresql-${POSTGRESQL_VERSION}.jar -f
 curl http://repo1.maven.org/maven2/com/microsoft/sqlserver/mssql-jdbc/${MSSQL_VERSION}/mssql-jdbc-${MSSQL_VERSION}.jar -o ${APP_ROOT}/db-libs/mssql-jdbc-${MSSQL_VERSION}.jar -f
+
+echo "Downloading MQ drivers to ${APP_ROOT}/mq-libs"
+mkdir ${APP_ROOT}/mq-libs
+
+curl https://repo1.maven.org/maven2/com/rabbitmq/amqp-client/5.7.3/amqp-client-5.7.3.jar -o ${APP_ROOT}/mq-libs/amqp-client-5.7.3.jar -f
+curl https://repo1.maven.org/maven2/com/rabbitmq/jms/rabbitmq-jms/1.13.0/rabbitmq-jms-1.13.0.jar -o ${APP_ROOT}/mq-libs/rabbitmq-jms-1.13.0.jar
+curl https://repo1.maven.org/maven2/org/apache/activemq/activemq-all/5.15.9/activemq-all-5.15.9.jar -o ${APP_ROOT}/mq-libs/activemq-all-5.15.9.jar
