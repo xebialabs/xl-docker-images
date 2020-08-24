0aae35e76885ed04c3c9c973d79fc49eeea81dad
dineshbalajis <dinesh@collab.net>
2020-08-24 18:25:41 +0530

    Fix: 1. Modify the tcp port of loadbalancer to point to 5000 instead of 8080. ( UI can still be accessed through 8080) 2. Expose the Loadbalancer port from the main run.sh script so that it can be re-used for master script.

--- docs/xl-deploy-ha/run.sh
+++ docs/xl-deploy-ha/run.sh
@@ -15,6 +15,7 @@ docker-compose -f docker-compose-xld-ha.yaml up --scale xl-deploy-master=2 -d ||
 # get the IP of master nodes, change the container names if you are not inside a folder named "xl-deploy-ha"
 export XLD_MASTER_1=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' xl-deploy-ha_xl-deploy-master_1)
 export XLD_MASTER_2=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' xl-deploy-ha_xl-deploy-master_2)
+export XLD_LB=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' xl-deploy-lb)
 
 echo ">>> Deploy worker nodes"
 # Deploy the worker nodes, you can change the number of nodes here if you wish
