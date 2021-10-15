a90325fbe3ee5e91dcf2b54290a8dd8321be2d55
bnechyporenko <bogdannechiporenko@gmail.com>
2021-10-15 13:32:10 +0200

    wip

--- docs/xl-deploy-ha/slim-down.sh
+++ docs/xl-deploy-ha/slim-down.sh
@@ -2,9 +2,15 @@ docker-compose -f docker-compose-xld-ha.yaml -f docker-compose-xld-ha-slim-worke
 
 # You can uncomment the next list of commands if you want to clean up the mounted volumes.
 
-#rm -rf $PWD/xl-deploy-master/conf/*
-#rm -rf $PWD/xl-deploy-master/centralConfiguration/*
-#rm -rf $PWD/xl-deploy-master/plugins/*
-#
-#rm -rf $PWD/xl-deploy-worker/conf/*
-#rm -rf $PWD/xl-deploy-worker/plugins/*
+if [[ $# -eq 1 ]] ; then
+  if [ "$1" == 'clean' ]; then
+    echo "Cleaning mounted volumes."
+    rm -rf $PWD/xl-deploy-master/conf/*
+    rm -rf $PWD/xl-deploy-master/centralConfiguration/*
+    rm -rf $PWD/xl-deploy-master/plugins/*
+
+    rm -rf $PWD/xl-deploy-worker/conf/*
+    rm -rf $PWD/xl-deploy-worker/plugins/*
+    echo "Mounted volumes has cleaned successfully."
+  fi
+fi
