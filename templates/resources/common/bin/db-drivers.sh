b2b47478d17814a59e73d67692a4b0bbbb69f403
Miras Mahamood <miras.mahamood@digital.ai>
2022-03-24 16:44:51 +0530

    ENG-9371 Upgrade libraries to fix CVE vulnerability

--- templates/resources/common/bin/db-drivers.sh
+++ templates/resources/common/bin/db-drivers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-MYSQL_VERSION="5.1.49"
-H2_VERSION="1.4.200"
-POSTGRESQL_VERSION="42.2.18"
-MSSQL_VERSION="9.4.0.jre11"
+MYSQL_VERSION="8.0.28"
+H2_VERSION="2.1.210"
+POSTGRESQL_VERSION="42.3.3"
+MSSQL_VERSION="10.2.0.jre11"
 
 echo "Downloading DB drivers to ${APP_ROOT}/db-libs"
 mkdir ${APP_ROOT}/db-libs
@@ -17,9 +17,9 @@ curl https://repo1.maven.org/maven2/com/microsoft/sqlserver/mssql-jdbc/${MSSQL_V
 echo "Downloading MQ drivers to ${APP_ROOT}/mq-libs"
 mkdir ${APP_ROOT}/mq-libs
 
-RABBIT_MQ_AMQP_VERSION="5.7.3"
-RABBIT_MQ_JMS_VERSION="1.13.0"
-ACTIVE_MQ_VERSION="5.15.9"
+RABBIT_MQ_AMQP_VERSION="5.14.2"
+RABBIT_MQ_JMS_VERSION="2.4.0"
+ACTIVE_MQ_VERSION="5.17.0"
 
 curl https://repo1.maven.org/maven2/com/rabbitmq/amqp-client/${RABBIT_MQ_AMQP_VERSION}/amqp-client-${RABBIT_MQ_AMQP_VERSION}.jar -o ${APP_ROOT}/mq-libs/amqp-client-${RABBIT_MQ_AMQP_VERSION}.jar -f
 curl https://repo1.maven.org/maven2/com/rabbitmq/jms/rabbitmq-jms/${RABBIT_MQ_JMS_VERSION}/rabbitmq-jms-${RABBIT_MQ_JMS_VERSION}.jar -o ${APP_ROOT}/mq-libs/rabbitmq-jms-${RABBIT_MQ_JMS_VERSION}.jar
