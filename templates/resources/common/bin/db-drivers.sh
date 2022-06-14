298000bf72e1aeec2b48915e856a5749018c97d6
Miras Mahamood <miras.mahamood@digital.ai>
2022-06-14 10:45:39 +0530

    S-85431 Upgrade libraries version

--- templates/resources/common/bin/db-drivers.sh
+++ templates/resources/common/bin/db-drivers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-MYSQL_VERSION="8.0.28"
-H2_VERSION="2.1.210"
-POSTGRESQL_VERSION="42.3.3"
-MSSQL_VERSION="10.2.0.jre11"
+MYSQL_VERSION="8.0.29"
+H2_VERSION="2.1.212"
+POSTGRESQL_VERSION="42.3.6"
+MSSQL_VERSION="10.2.1.jre11"
 
 echo "Downloading DB drivers to ${APP_ROOT}/db-libs"
 mkdir ${APP_ROOT}/db-libs
@@ -19,7 +19,7 @@ mkdir ${APP_ROOT}/mq-libs
 
 RABBIT_MQ_AMQP_VERSION="5.14.2"
 RABBIT_MQ_JMS_VERSION="2.4.0"
-ACTIVE_MQ_VERSION="5.17.0"
+ACTIVE_MQ_VERSION="5.17.1"
 
 curl https://repo1.maven.org/maven2/com/rabbitmq/amqp-client/${RABBIT_MQ_AMQP_VERSION}/amqp-client-${RABBIT_MQ_AMQP_VERSION}.jar -o ${APP_ROOT}/mq-libs/amqp-client-${RABBIT_MQ_AMQP_VERSION}.jar -f
 curl https://repo1.maven.org/maven2/com/rabbitmq/jms/rabbitmq-jms/${RABBIT_MQ_JMS_VERSION}/rabbitmq-jms-${RABBIT_MQ_JMS_VERSION}.jar -o ${APP_ROOT}/mq-libs/rabbitmq-jms-${RABBIT_MQ_JMS_VERSION}.jar
