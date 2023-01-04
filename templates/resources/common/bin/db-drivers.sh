5fe7a693425faae9ccc683fe0d8dcdac6de55e89
Mayur Patel <mayur9991patel@hotmail.com>
2023-01-04 20:40:00 +0530

    D-22950 Update db and mq libs version

--- templates/resources/common/bin/db-drivers.sh
+++ templates/resources/common/bin/db-drivers.sh
@@ -3,8 +3,8 @@ set -e
 
 MYSQL_VERSION="5.1.49"
 H2_VERSION="1.4.200"
-POSTGRESQL_VERSION="42.2.18"
-MSSQL_VERSION="9.4.0.jre11"
+POSTGRESQL_VERSION="42.5.1"
+MSSQL_VERSION="9.4.1.jre11"
 
 echo "Downloading DB drivers to ${APP_ROOT}/db-libs"
 mkdir ${APP_ROOT}/db-libs
@@ -17,9 +17,9 @@ curl https://repo1.maven.org/maven2/com/microsoft/sqlserver/mssql-jdbc/${MSSQL_V
 echo "Downloading MQ drivers to ${APP_ROOT}/mq-libs"
 mkdir ${APP_ROOT}/mq-libs
 
-RABBIT_MQ_AMQP_VERSION="5.7.3"
-RABBIT_MQ_JMS_VERSION="1.13.0"
-ACTIVE_MQ_VERSION="5.15.9"
+RABBIT_MQ_AMQP_VERSION="5.16.0"
+RABBIT_MQ_JMS_VERSION="1.15.2"
+ACTIVE_MQ_VERSION="5.17.3"
 
 curl https://repo1.maven.org/maven2/com/rabbitmq/amqp-client/${RABBIT_MQ_AMQP_VERSION}/amqp-client-${RABBIT_MQ_AMQP_VERSION}.jar -o ${APP_ROOT}/mq-libs/amqp-client-${RABBIT_MQ_AMQP_VERSION}.jar -f
 curl https://repo1.maven.org/maven2/com/rabbitmq/jms/rabbitmq-jms/${RABBIT_MQ_JMS_VERSION}/rabbitmq-jms-${RABBIT_MQ_JMS_VERSION}.jar -o ${APP_ROOT}/mq-libs/rabbitmq-jms-${RABBIT_MQ_JMS_VERSION}.jar
