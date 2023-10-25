d6973c4fd87c0c9574646d40ac75a1a7a78e09e5
nelson-digi <88083340+nelson-digi@users.noreply.github.com>
2023-10-25 15:07:30 +0530

    Upgrade DB and JMS version, compatible with JDK17 version (#196)
    * Upgrade DB and JMS version, compatible with JDK17 version
    
    * Updating activemq client
    
    ---------
    
    Co-authored-by: Ishwarya Surendrababu <sishwarya@digital.ai>

--- templates/resources/common/bin/db-drivers.sh
+++ templates/resources/common/bin/db-drivers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-MYSQL_VERSION="8.0.31"
-H2_VERSION="2.2.220"
-POSTGRESQL_VERSION="42.5.1"
-MSSQL_VERSION="11.2.2.jre11"
+MYSQL_VERSION="8.1.0"
+H2_VERSION="2.2.224"
+POSTGRESQL_VERSION="42.6.0"
+MSSQL_VERSION="11.2.3.jre17"
 
 echo "Downloading DB drivers to ${APP_ROOT}/db-libs"
 mkdir ${APP_ROOT}/db-libs
@@ -17,10 +17,10 @@ curl https://repo1.maven.org/maven2/com/microsoft/sqlserver/mssql-jdbc/${MSSQL_V
 echo "Downloading MQ drivers to ${APP_ROOT}/mq-libs"
 mkdir ${APP_ROOT}/mq-libs
 
-RABBIT_MQ_AMQP_VERSION="5.16.0"
-RABBIT_MQ_JMS_VERSION="2.8.0"
-ACTIVE_MQ_VERSION="5.17.3"
+RABBIT_MQ_AMQP_VERSION="5.18.0"
+RABBIT_MQ_JMS_VERSION="3.1.0"
+ACTIVE_MQ_VERSION="5.18.2"
 
 curl https://repo1.maven.org/maven2/com/rabbitmq/amqp-client/${RABBIT_MQ_AMQP_VERSION}/amqp-client-${RABBIT_MQ_AMQP_VERSION}.jar -o ${APP_ROOT}/mq-libs/amqp-client-${RABBIT_MQ_AMQP_VERSION}.jar -f
 curl https://repo1.maven.org/maven2/com/rabbitmq/jms/rabbitmq-jms/${RABBIT_MQ_JMS_VERSION}/rabbitmq-jms-${RABBIT_MQ_JMS_VERSION}.jar -o ${APP_ROOT}/mq-libs/rabbitmq-jms-${RABBIT_MQ_JMS_VERSION}.jar
-curl https://repo1.maven.org/maven2/org/apache/activemq/activemq-all/${ACTIVE_MQ_VERSION}/activemq-all-${ACTIVE_MQ_VERSION}.jar -o ${APP_ROOT}/mq-libs/activemq-all-${ACTIVE_MQ_VERSION}.jar
+curl https://repo1.maven.org/maven2/org/apache/activemq/activemq-client-jakarta/${ACTIVE_MQ_VERSION}/activemq-client-jakarta-${ACTIVE_MQ_VERSION}.jar -o ${APP_ROOT}/mq-libs/activemq-client-jakarta-${ACTIVE_MQ_VERSION}.jar
